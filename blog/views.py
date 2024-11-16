c60adcd53318e70395c28aa37aa8c51e4d03f5a1
Fake da Silva <falso@teste.com>
2024-11-16 12:44:49 -0300

    Apenas com views funcionais, utilizando um Django form;

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
+from .forms import PostForm
 
 
 def lista_posts(request):
@@ -14,21 +15,27 @@ def detalha_post(request, post_id):
 
 def cria_post(request):
     if request.method == 'POST':
-        titulo = request.POST.get('titulo')
-        conteudo = request.POST.get('conteudo')
-        post = Post.objects.create(titulo=titulo, conteudo=conteudo)
-        return redirect('detalha_post', post_id=post.id)
-    return render(request, 'blog/cria_post.html')
+        form = PostForm(request.POST)
+        if form.is_valid():
+            form.save()  
+            return redirect('lista_posts')
+    else:
+        form = PostForm()
+
+    return render(request, 'blog/cria_post.html', {'form': form})
 
 
 def edita_post(request, post_id):
-    post = get_object_or_404(Post, id=post_id)
+    post = Post.objects.get(id=post_id)
     if request.method == 'POST':
-        post.titulo = request.POST.get('titulo')
-        post.conteudo = request.POST.get('conteudo')
-        post.save()
-        return redirect('detalha_post', post_id=post.id)
-    return render(request, 'blog/edita_post.html', {'post': post})
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect('detalha_post', post_id=post.id)
+    else:
+        form = PostForm(instance=post)
+
+    return render(request, 'blog/edita_post.html', {'form': form, 'post': post})
 
 
 def deleta_post(request, post_id):
